d4a2f6d67b4ed0e03d21317084d5a54de30b6131
higor-xavier <higorsxavier@gmail.com>
2023-05-06 00:13:00 -0300

    Modeling the dashboard class

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(document).ready(() => {
 	
+	$('#documentacao').on('click',() => {
+		// $('#pagina').load('documentacao.html')
+		// $.get('documentacao.html', data => {
+		// 	$('#pagina').html(data)
+		// })
+		$.post('documentacao.html', data => {
+			$('#pagina').html(data)
+		})
+	})
+
+	$('#suporte').on('click',data => {
+		// $('#pagina').load('suporte.html')
+		// $.get('suporte.html', data => {
+		// 	$('#pagina').html(data)
+		// })
+		$.post('suporte.html', data => {
+			$('#pagina').html(data)
+		})
+
+	})
+
 })
